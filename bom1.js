053341bbb60eb178d2828907ec8cf0496b768728
mdmojafforhossen <mdmojaffor98@gmail.com>
2024-09-02 22:55:22 +0600

    BOM class one end

---
+++ bom1.js
@@ -0,0 +1,22 @@
+
+
+
+// var locations = document.getElementById("myButton");
+
+// locations.addEventListener("click",function(){
+//   location.assign("https://www.w3schools.com")
+// });
+
+var locations = document.getElementById("myButton");
+locations.addEventListener("click",function(){
+    document.write(window.location.href);
+});
+// locations.addEventListener("click",function(){
+//     document.write(window.location.hostname);
+// });
+// locations.addEventListener("click",function(){
+//     document.write(window.location.pathname);
+// });
+// locations.addEventListener("click",function(){
+//     document.write(window.location.protocol);
+// });
