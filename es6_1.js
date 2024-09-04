203c32aeb983868803c82b8d441b3925b076527b
mdmojafforhossen <mdmojaffor98@gmail.com>
2024-09-04 08:00:14 +0600

    ES6 class 1 end

---
+++ es6_1.js
@@ -0,0 +1,72 @@
+
+
+// simple variablr
+// simple variablr
+
+// var x = 10;
+
+// if (true){
+//     var x = 5;
+// }
+
+// console.log(x);
+
+//    ES6 variable
+//    ES6 variable
+
+
+// let x = 10;
+
+// if(true){
+//     let x =7;
+// }
+
+// console.log(x);
+
+
+// const varibale
+// const varibale
+
+// const x = 10;
+
+
+// if(true){
+//      x =6;
+// }
+
+// console.log(x);
+
+// String concat 
+// String concat 
+
+// let x =6;
+// let y= 5;
+
+// let sum = x+y;
+
+// console.log(`Total Number = ${sum} the end of number`);
+
+// let firstName = "Mojaffor";
+// let lastName = " Hossen"
+
+// let fullName = `my name is  ${firstName + lastName } i'm a teacher`;
+
+// console.log(fullName);
+
+// function decleare
+// function decleare
+
+// function totalSum(x,y){
+//     var sum = x+y ;
+//     console.log(sum);
+// }
+
+// totalSum(20,30);
+
+//ES6 function decleare
+//ES6 function decleare
+// const totalSum = (x ,y)=>{
+//     let sum = x+y ;
+//     console.log(sum);
+// }
+// totalSum(30,30);
