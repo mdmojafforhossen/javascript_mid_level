0b7e5ab7040feab2d4d9e3af7d51bba157bcc6ef
mdmojafforhossen <mdmojaffor98@gmail.com>
2024-09-05 11:06:01 +0600

    ES6 class 4 compelete

---
+++ es6_4.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+// let number = [1,2,3,4]
+// let number2 = [5,6,7,8]
+
+// console.log(...number, ...number2);
+
+let p1 = {
+    name :"mojaffor hossen",
+    age :29,
+    Nationality :"Bangladeshi",
+}
+
+let p2 = {
+    GPA :5.00,
+    Institute :"National University",
+    Group :"Humanity"
+}
+
+console.log({...p1, ...p2});
