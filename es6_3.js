9dfe8a88e5d38881fe6cb2d2cd76eeabdca186ea
mdmojafforhossen <mdmojaffor98@gmail.com>
2024-09-05 10:23:15 +0600

    class 3 complete

---
+++ es6_3.js
@@ -0,0 +1,20 @@
+
+// default function 
+// default function 
+// function myMessage(text='default text'){
+
+//     console.log(text);
+// }
+
+// myMessage();
+// myMessage("this is md mojaffor hoeen");
+
+// rest perametar 
+// rest perametar 
+
+function myNumber(x,y, ...z){
+    console.log(`x= ${x}, y= ${y}, z= ${z}`)
+
+}
+
+myNumber(20,30,40,50,60,70,80,90);
