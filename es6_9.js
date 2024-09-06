49ec7479981978d9e3f15a0e11026830675d6f50
mdmojafforhossen <mdmojaffor98@gmail.com>
2024-09-06 15:03:19 +0600

    arrow function part one complete

---
+++ es6_9.js
@@ -0,0 +1,32 @@
+// arrow function 
+// arrow function 
+
+
+// function display1(){console.log("I'm traditional function")}
+
+// var display2 =()=>console.log("I'm arrow function");
+
+
+// display1();
+
+// display2();
+
+// function message1(){
+//     return "i'm traditional function"
+// }
+// console.log(message1());
+
+
+// var message2 =()=>"I'm arrow function";
+
+// console.log(message2());
+
+function add1(x,y){
+    return x+y;
+}
+
+var add2 = (num,num2) => num+num2;
+
+console.log(add1(20,30));
+
+console.log(add2(50,50));
